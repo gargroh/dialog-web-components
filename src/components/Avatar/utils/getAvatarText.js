eb1e5c57abbd60efe7864de925ce7b06648c6b8e
Elina Denisova <elin.rinnel@gmail.com>
2019-07-13 10:40:45 +0300

    fix(Avatar): delete logs

--- src/components/Avatar/utils/getAvatarText.js
+++ src/components/Avatar/utils/getAvatarText.js
@@ -10,16 +10,12 @@ import { parse, decorators } from '@dlghq/markdown';
 function getAvatarText(title: string): string {
   if (title && title.length) {
     const titleContent = parse(title, decorators)[0].content;
-
-    console.log(titleContent);
-    const titleArray = titleContent.reduce((array, {content, highlight} ) => {
+    const titleArray = titleContent.reduce((array, { content, highlight }) => {
       if (!highlight) {
-        console.log(console.log(titleContent));
-        return [...array, ...content.trim().split(' ')] ;
+        return [...array, ...content.trim().split(' ')];
       }
       return array;
     }, []);
-    console.log(titleArray);
 
     if (titleArray.length === 1) {
       return titleArray[0][0] || '#';
